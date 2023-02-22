f3aeb25d7451e5d66b352c3a7d1d344d2756f81b
Simisola Omonayajo <simisola.omonayajo@gmail.com>
2023-02-22 12:11:14 +0100

    update

--- CNN/settings.py
+++ CNN/settings.py
@@ -62,6 +62,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CNN.wsgi
